321d279cec9aef4610f10b489462046983143d40
palexa <pytsik@mail.ru>
2018-04-02 17:09:46 +0300

    node server

--- server.js
+++ server.js
@@ -1,44 +1,13 @@
-var util =require('util');
- var phrases={
-   "Hello":"Привет",
-   "World":"Мир"
- };
+var http =require('http');
+var server=new http.Server();
+server.listen(1488,'127.0.0.1');
+var count=0;
 
- function PhraseError(message) {
-     this.message=message;
-     Error.captureStackTrace(this,PhraseError);
- }
- util.inherits(PhraseError,Error);
- PhraseError.prototype.name='PhraseError';
- function HttpError(error,text) {
-     this.status=error;
-     this.message=text;
-     Error.captureStackTrace(this,HttpError);
- }
-util.inherits(HttpError,Error);
-HttpError.prototype.name='HttpError';
-function getPhrase(name) {
-    if(!phrases[name]){
-        throw new PhraseError("No such phrase"+name);
-    }
-    return phrases[name];
-}
-function makePage(url) {
-    if(url != 'index.html'){
-        throw new HttpError(404,"Page doen't exist");
-    }
-    return util.format("%s, %s!",getPhrase("******"),getPhrase("World"));
-}
-
-try {
-    var page = makePage('index.html');
-    console.log(page);
-}
-catch (e){
-     if (e instanceof HttpError){
-         console.log(e.status,e.message);
-     }
-     else {
-         console.error('Error %s\n message: %s\n stack: %s',e.name,e.message,e.stack);
-     }
-}
+var emit=server.emit;
+server.emit=function (event) {
+    console.log(event);
+    emit.apply(server,arguments);
+};
+server.on('request',function (req,res) {
+    res.end("Hello world " + ++count);
+});
