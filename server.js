d578a50de5a3d9eb2f3462dcef8476a7ec3da6a4
palexa <pytsik@mail.ru>
2018-04-03 23:11:38 +0300

    EchoServer

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var http =require('http');
 var url=require('url');
 var server=new http.Server(function (req,res) {
-console.log(req.method,req.url);
+console.log(req.headers);
 
 var urlParsed=url.parse(req.url,true);
-console.log(urlParsed);
+
 if (urlParsed.pathname='/echo'&&urlParsed.query.message){
-     res.end(urlParsed.query.message);
+    res.setHeader('Cache-control','no-cache');
+    res.end(urlParsed.query.message);
 }
 else{
     res.statusCode=404;
