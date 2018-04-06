5bf51ac87c684c646804ba6699be911f0fead5a1
palexa <pytsik@mail.ru>
2018-04-06 17:33:16 +0300

    some changes

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-var http =require('http');
-var url=require('url');
-var server=new http.Server(function (req,res) {
-console.log(req.headers);
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
-var urlParsed=url.parse(req.url,true);
-
-if (urlParsed.pathname='/echo'&&urlParsed.query.message){
-    res.setHeader('Cache-control','no-cache');
-    res.end(urlParsed.query.message);
-}
-else{
-    res.statusCode=404;
-    res.end("Page not found");
-}
-//res.end("Suka kirill");
-});
-server.listen(1388,'127.0.0.1');
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(9090);
