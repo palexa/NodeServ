5d95ee59747c0ee4720a7b8f946f1957a66b2247
palexa <pytsik@mail.ru>
2018-04-22 20:12:44 +0300

    express

--- server.js
+++ server.js
@@ -1,68 +1,33 @@
-// var http = require('http');
-// var url = require('url');
-// var fs = require('fs');
-// var express=require('express');
-// var app=new express();
-//
-// app.use("/static", express.static(__dirname + "/public"));
-//
-// app.get("/", function(request, response){
-//
-//     response.send("<h1>Главная страница</h1>");
-// });
-// app.get("/contact", function(request, response){
-//
-//     response.send("<h1>Контакты</h1>");
-// });
-// app.listen(3000);
+
 var express = require("express");
-var bodyParser = require("body-parser");
+var hbs=require("hbs");
 
 var app = express();
+app.use(express.static('static'));
+hbs.registerPartials(__dirname + "/views/partials");
 
-// создаем парсер для данных application/x-www-form-urlencoded
-var urlencodedParser = bodyParser.json({extended: false});
-
-app.use(express.static(__dirname + "/Pages"));
-
-app.post("/user", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
-    response.json(`${request.body.userName} - ${request.body.userAge}`);
-});
+app.set("view engine", "hbs");
 
 app.get("/", function(request, response){
-    response.send("<h1>Главная страница</h1>");
+
+    response.render("home.hbs");
+});
+app.get("/contact", function(request, response){
+
+    response.render("contact.hbs", {
+        title: "Мои контакты",
+        emailsVisible: true,
+        emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+        phone: "+1234567890"
+    });
+});
+app.get("/index", function(request, response){
+    response.render("index.hbs");
+});
+app.get("/twitter", function(request, response){
+    response.render("twitter.hbs");
 });
 app.listen(3000);
 
 
 
-/*
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    console.log(q.pathname);
-    if(q.pathname.substr(-4,4)=='html'){
-        var filename = "./Pages/" + q.pathname;
-    }
-    else{
-        var filename = "." + q.pathname;
-    }
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {
-                'Content-Type': 'text/html'
-            });
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
-            'Cache-Control': 'no-cache, no-store, must-revalidate',
-            'Pragma': 'no-cache',
-            'Expires': '0'
-        });
-        res.write(data);
-        return res.end();
-    });
-}).listen(9090);
-*/
